1fa8769dcc2f9f20e3b1d04803729f7b6858bfca
Jiho Park <diorpark@gmail.com>
2023-07-15 18:06:04 +0900

    Feat: redux-persist 패키지 설치 및 적용

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,14 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer, { selectUserState } from "./reducers/userSlice";
 import postReducer, { selectPostState } from "./reducers/postSlice";
 import commentReducer, { selectCommentState } from "./reducers/commentSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  post: postReducer,
+  comment: commentReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    post: postReducer,
-    comment: commentReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -18,4 +29,5 @@ export const selectUser = (state: RootState) => selectUserState(state).user;
 export const selectPost = (state: RootState) => selectPostState(state).posts;
 export const selectComment = (state: RootState) => selectCommentState(state).comments;
 
+export const persistor = persistStore(store);
 export default store;
