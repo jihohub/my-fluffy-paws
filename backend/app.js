d259c3790095d5b953c3319ed4f9bc213b3de26f
Jetty <diorpark@gmail.com>
2023-10-10 13:35:44 +0900

    Feat: 비로그인 상태에서 내비게이션 채팅 버튼 클릭시 로그인 페이지로 이동

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ const httpServer = http.createServer(app);
 const initializeSocketIO = require("./socket");
 const io = initializeSocketIO(httpServer);
 
-httpServer.listen(5000, () => console.log(`App listening on port 5000!`));
+httpServer.listen(5001, () => console.log(`App listening on port 5001!`));
 
 // Routes
 app.use("/api/user", userRouter);
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:5000", // 프록시할 백엔드 서버의 주소
+      target: "http://ec2-13-125-3-209.ap-northeast-2.compute.amazonaws.com", // 프록시할 백엔드 서버의 주소
       changeOrigin: true,
     })
   );
